e1e7a035f5427448384e7708383fd08c3aa3396f
Artem Solovev <artem.solovev.lse@gmail.com>
2017-06-28 13:22:44 +0300

    Added ability to save data in edited fields

--- 11 - excel/app.js
+++ 11 - excel/app.js
@@ -1,18 +1,3 @@
-var headers = [
-  "Book", "Author", "Language", "Published", "Sales"
-];
-
-var data = [
-  ["The Lord of the Rings", "J. R. R. Tolkien", "English", "1954-1955", "150 million"],
-  ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry", "French", "1943", "140 million"],
-  ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"],
-  ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
-  ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754-1791", "100 million"],
-  ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"],
-  ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"],
-];
-
-
 var Excel = React.createClass({
   displayName: 'Excel',
 
@@ -26,13 +11,32 @@ var Excel = React.createClass({
       )
     ),
   },
-  getInitialState() {
+
+  getInitialState: function() {
     return {
       data: this.props.initialData,
       sortby: null,
       descending: false,
-      edit: null,
+      edit: null, // [row index, cell index]
     };
+  },
+
+  _sort: function(e) {
+    var column = e.target.cellIndex;
+    var data = this.state.data.slice();
+    var descending = this.state.sortby === column && !this.state.descending;
+
+    data.sort(function(a, b) {
+      return descending ?
+        (a[column] < b[column] ? 1 : -1) :
+        (a[column] > b[column] ? 1 : -1);
+    });
+
+    this.setState({
+      data: data,
+      sortby: column,
+      descending: descending,
+    });
   },
 
   _showEditor: function(e) {
@@ -44,19 +48,15 @@ var Excel = React.createClass({
     });
   },
 
-  _sort: function(e) {
-    var column = e.target.cellIndex;
+  _save: function(e) {
+    e.preventDefault();
+    var input = e.target.firstChild;
     var data = this.state.data.slice();
-    var descending = this.state.sortby === column && !this.state.descending;
-    data.sort(function(a, b) {
-      return descending ?
-        (a[column] < b[column] ? 1 : -1) :
-        (a[column] > b[column] ? 1 : -1);
-    });
+    data[this.state.edit.row][this.state.edit.cell] = input.value;
+
     this.setState({
+      edit: null,
       data: data,
-      sortby: column,
-      descending: descending,
     });
   },
 
@@ -73,7 +73,6 @@ var Excel = React.createClass({
               if (this.state.sortby === idx) {
                 title += this.state.descending ? ' \u2191' : ' \u2193'
               }
-
               return React.DOM.th({
                 key: idx
               }, title);
@@ -90,20 +89,44 @@ var Excel = React.createClass({
                 },
                 row.map(function(cell, idx) {
                   var content = cell;
-
+                  var edit = this.state.edit;
+                  if (edit && edit.row === rowidx && edit.cell === idx) {
+                    content = React.DOM.form({
+                        onSubmit: this._save
+                      },
+                      React.DOM.input({
+                        type: 'text',
+                        defaultValue: cell,
+                      })
+                    );
+                  }
                   return React.DOM.td({
                     key: idx,
-                    'data-row': rowidx
+                    'data-row': rowidx,
                   }, content);
                 }, this)
               )
             );
-          })
+          }, this)
         )
       )
     );
   }
 });
+
+var headers = [
+  "Book", "Author", "Language", "Published", "Sales"
+];
+
+var data = [
+  ["The Lord of the Rings", "J. R. R. Tolkien", "English", "1954-1955", "150 million"],
+  ["Le Petit Prince (The Little Prince)", "Antoine de Saint-Exupéry", "French", "1943", "140 million"],
+  ["Harry Potter and the Philosopher's Stone", "J. K. Rowling", "English", "1997", "107 million"],
+  ["And Then There Were None", "Agatha Christie", "English", "1939", "100 million"],
+  ["Dream of the Red Chamber", "Cao Xueqin", "Chinese", "1754-1791", "100 million"],
+  ["The Hobbit", "J. R. R. Tolkien", "English", "1937", "100 million"],
+  ["She: A History of Adventure", "H. Rider Haggard", "English", "1887", "100 million"],
+];
 
 ReactDOM.render(
   React.createElement(Excel, {
