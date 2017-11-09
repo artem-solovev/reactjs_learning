63260064abf8b43a65bc93fd0b464c8e473e3f78
Artem Solovev <artem.solovev.lse@gmail.com>
2017-11-09 12:09:02 +0300

    Added initial workflow for news app

---
+++ learn_javascript/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {render} from 'react-dom'
+
+
+function HelloWorld() {
+    return <h1>Hello World!</h1>
+}
+
+
+render(
+    <HelloWorld />,
+    document.getElementById('container')
+)
