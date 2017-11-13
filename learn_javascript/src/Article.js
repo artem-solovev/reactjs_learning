df53fd53494635f1f46886e2e79cbd132bdb172a
Artem Solovev <artem.solovev.lse@gmail.com>
2017-11-13 11:28:14 +0300

    Added ability to show comments list

--- learn_javascript/src/Article.js
+++ learn_javascript/src/Article.js
@@ -1,15 +1,70 @@
 import React, { Component } from 'react'
 
 export default class Article extends Component {
+    constructor( props ) {
+        super( props );
+
+        this.state = {
+            isArticleOpen: false,
+            isCommentsOpen: false
+        }
+    }
+
     render() {
-        const {article} = this.props;
+        const { article } = this.props;
+        const { comments } = article;
+
+        const { isArticleOpen } = this.state;
+        const { isCommentsSectionOpen } = this.state;
 
         return (
             <div>
                 <h3>{ article.title }</h3>
-                <section>{ article.text }</section>
+                <button onClick = { this.toggleArticleOpen }>open</button>
+                { this.getArticleBody() }
             </div>
         )
     }
+
+    getArticleBody() {
+        if ( !this.state.isArticleOpen ) {
+            return null;
+        }
+
+        const { article } = this.props;
+
+        return <div>
+                    <section>{ article.text }</section>
+                    <a href="#" onClick = { this.toggleCommentsOpen }>Show comments</a>
+                    { this.getCommentsBody() }
+                </div>;
+    }
+
+    getCommentsBody() {
+        if ( !this.state.isCommentsSectionOpen ) {
+            return null;
+        }
+        const { comments } = this.props.article;
+
+        const commentsElements = comments.map( (comment) => <li key = {comment.id}><i>{ comment.user }</i><br />{ comment.text }</li> )
+        
+        return (
+            <ul>
+                {commentsElements}
+            </ul>
+        )
+    }
+
     
+    toggleArticleOpen = () => {
+        this.setState( {
+            isArticleOpen: !this.state.isArticleOpen
+        } )
+    }
+
+    toggleCommentsOpen = () => {
+        this.setState( {
+            isCommentsSectionOpen: !this.state.isCommentsSectionOpen
+        } )
+    }
 }
