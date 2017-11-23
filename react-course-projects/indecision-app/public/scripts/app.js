44770daaa31cdd7f90a5e07ae07764c65ac7ff7e
Artem Solovev <artem.solovev.lse@gmail.com>
2017-11-23 16:34:46 +0300

    Added MAIN APP file

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -1,1 +1,11 @@
 console.log( "Here it is!!!" );
+
+var template = React.createElement(
+    "h1",
+    { id: "someId" },
+    "Something new"
+);
+
+var appRoot = document.getElementById( "app" );
+
+ReactDOM.render( template, appRoot );
