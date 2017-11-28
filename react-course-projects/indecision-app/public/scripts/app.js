a589b2e12360dd95681ab7ac4ac2795810c71245
Artem Solovev <artem.solovev.lse@gmail.com>
2017-11-28 11:46:01 +0300

    Added simple counter with no rendering result

--- react-course-projects/indecision-app/public/scripts/app.js
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -22,6 +22,55 @@ var template = React.createElement(
     )
 );
 
+var count = 0;
+
+var addOne = function addOne() {
+    count++;
+    console.log('addOne ', count);
+};
+
+var minusOne = function minusOne() {
+    count--;
+    console.log('minusOne ', count);
+};
+
+var reset = function reset() {
+    count = 0;
+    console.log('reset ', count);
+};
+
+var templateTwo = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Count: ",
+        count,
+        " "
+    ),
+    React.createElement(
+        "button",
+        { onClick: addOne },
+        "+1"
+    ),
+    React.createElement(
+        "button",
+        { onClick: minusOne },
+        "-1"
+    ),
+    React.createElement(
+        "button",
+        { onClick: reset },
+        "reset"
+    ),
+    React.createElement(
+        "p",
+        null,
+        count
+    )
+);
+
 var appRoot = document.getElementById("app");
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- react-course-projects/indecision-app/src/app.js
+++ react-course-projects/indecision-app/src/app.js
@@ -10,6 +10,33 @@ var template = <div>
                     <p>{ app.subtitle }</p>
                 </div>;
 
+let count = 0;
+
+const addOne = () => {
+    count++;
+    console.log( 'addOne ', count );
+};
+
+const minusOne = () => {
+    count--;
+    console.log( 'minusOne ', count );
+};
+
+const reset = () => {
+    count = 0;
+    console.log( 'reset ', count );
+};
+
+var templateTwo = (
+    <div>
+        <h1>Count: { count } </h1>
+        <button onClick = { addOne }>+1</button>
+        <button onClick = { minusOne }>-1</button>
+        <button onClick = { reset }>reset</button>
+        <p>{count}</p>
+    </div>
+);
+
 var appRoot = document.getElementById( "app" );
 
-ReactDOM.render( template, appRoot );
+ReactDOM.render( templateTwo, appRoot );
