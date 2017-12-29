010ce35b308eff206544b1b465e3f01887015a45
Artem Solovev <artem.solovev.lse@gmail.com>
2017-12-29 12:34:22 +0300

    Portfolio (redux): added ability to sub/un/subscribe to dispatch events

--- react-course-projects/portfolio/src/playground/redux-101.js
+++ react-course-projects/portfolio/src/playground/redux-101.js
@@ -3,12 +3,20 @@ import { createStore } from 'redux';
 const store = createStore( ( state = { count: 0 }, action ) => {
     switch ( action.type ) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
+            
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
+
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         case 'RESET':
             return {
@@ -19,10 +27,13 @@ const store = createStore( ( state = { count: 0 }, action ) => {
     }
 } );
 
-console.log( store.getState() );
+const unsubscribe = store.subscribe( () => {
+    console.log( store.getState() );
+} );
 
 store.dispatch( {
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 } );
 
 store.dispatch( {
@@ -30,11 +41,23 @@ store.dispatch( {
 } );
 
 store.dispatch( {
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decrementBy: 10
+} );
+
+
+store.dispatch( {
+    type: 'SET',
+    count: 101
 } );
 
 store.dispatch( {
     type: 'RESET'
 } );
 
-console.log( store.getState() );
+unsubscribe();
+
+store.dispatch( {
+    type: 'SET',
+    count: 101
+} );
