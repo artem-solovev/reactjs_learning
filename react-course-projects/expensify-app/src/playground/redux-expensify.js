d0bd3ec10ca2bcfacdb7fcddc46f51a067bb7601
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-02 09:59:40 +0300

    Expensify: added two functions and used es6 spread operator

--- react-course-projects/expensify-app/src/playground/redux-expensify.js
+++ react-course-projects/expensify-app/src/playground/redux-expensify.js
@@ -1,9 +1,40 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const addExpense = ( 
+                    { description = '',
+                    note = '',
+                    amount = 0,
+                    createdAt = 0 
+                } = {} ) => ( {
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+} );
+
+const removeExpense = ( { id } = {} ) => ( {
+    type: 'REMOVE_EXPENSE',
+    id
+} );
 
 const expenseReducerDefaultState = [];
 
 const expensesReducer = ( state = expenseReducerDefaultState, action ) => {
     switch ( action.type ) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+
+        case 'REMOVE_EXPENSE':
+            return state.filter( ( { id } ) => id !== action.id );
+
         default:
             return state;
     }
@@ -28,7 +59,14 @@ const store = createStore( combineReducers( {
     filters: filtersReducer
 } ) );
 
-console.log( store.getState() );
+store.subscribe( () => {
+    console.log( store.getState() );
+} );
+
+const expenseOne =  store.dispatch( addExpense( { description: 'Rent', amount: 100 } ) );
+const expenseTwo =  store.dispatch( addExpense( { description: 'Coffee', amount: 300 } ) );
+
+store.dispatch( removeExpense( { id: expenseOne.expense.id } ) );
 
 const demoState = {
     expenses: [ {
