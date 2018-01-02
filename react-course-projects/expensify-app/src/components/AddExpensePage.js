193b60fadfedacbfa98a2e16a0566ecfd29b1313
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-02 17:02:47 +0300

    Expensify: added input fields and basic functionality for adding expenses

--- react-course-projects/expensify-app/src/components/AddExpensePage.js
+++ react-course-projects/expensify-app/src/components/AddExpensePage.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import ExpenseForm from './ExpenseFrom';
 
 const AddExpensePage = () => (
-    <div>AddExpensePage</div>
+    <div>
+        <h2>Add expense</h2>
+        <ExpenseForm />
+    </div>
 );
 
 export default AddExpensePage;
