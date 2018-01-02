b5631712e4f46f958528fb938e53c7a77228e0d7
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-02 11:12:52 +0300

    Expensify: added some functions for filter

--- react-course-projects/expensify-app/src/playground/redux-expensify.js
+++ react-course-projects/expensify-app/src/playground/redux-expensify.js
@@ -33,6 +33,24 @@ const setTextFilter = ( text = '' ) => ( {
     text
 } );
 
+const sortByDate = () => ( {
+    type: 'SORT_BY_DATE'
+} );
+
+const sortByAmount = () => ( {
+    type: 'SORT_BY_AMOUNT'
+} );
+
+const setStartDate = ( startDate = '' ) => ( {
+    type: 'SET_START_DATE',
+    startDate
+} );
+
+const setEndDate = ( endDate = '' ) => ( {
+    type: 'SET_END_DATE',
+    endDate
+} );
+
 const expenseReducerDefaultState = [];
 
 const expensesReducer = ( state = expenseReducerDefaultState, action ) => {
@@ -77,6 +95,30 @@ const filtersReducer = ( state = filtersReducerDefaultState, action ) => {
                 ...state,
                 text: action.text
             };
+
+        case 'SORT_BY_AMOUNT':
+            return {
+                ...state,
+                sortBy: 'amount'
+            };
+        
+        case 'SORT_BY_DATE':
+            return {
+                ...state,
+                sortBy: 'date'
+            };
+
+        case 'SET_START_DATE':
+            return {
+                ...state,
+                startDate: action.startDate
+            };
+
+        case 'SET_END_DATE':
+            return {
+                ...state,
+                endDate: action.endDate
+            };
         default:
             return state;
     }
@@ -91,7 +133,7 @@ store.subscribe( () => {
     console.log( store.getState() );
 } );
 
-const expenseOne =  store.dispatch( addExpense( { description: 'Rent', amount: 100 } ) );
+/* const expenseOne =  store.dispatch( addExpense( { description: 'Rent', amount: 100 } ) );
 const expenseTwo =  store.dispatch( addExpense( { description: 'Coffee', amount: 300 } ) );
 
 store.dispatch( removeExpense( { id: expenseOne.expense.id } ) );
@@ -99,6 +141,13 @@ store.dispatch( editExpense( expenseTwo.expense.id, { amount: 500 } ) );
 
 store.dispatch( setTextFilter( 'rent' ) );
 store.dispatch( setTextFilter() );
+
+store.dispatch( sortByAmount() );
+store.dispatch( sortByDate() ); */
+
+store.dispatch( setStartDate( 125 ) );
+store.dispatch( setStartDate() );
+store.dispatch( setEndDate( 1250 ) );
 
 const demoState = {
     expenses: [ {
