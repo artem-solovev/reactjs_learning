9bc5cab9d8d767f9aa9e27aac826f0ebfae508f4
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-07 18:08:45 +0300

    Expensify: added first test for component

--- react-course-projects/expensify-app/src/tests/components/Header.test.js
+++ react-course-projects/expensify-app/src/tests/components/Header.test.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Header from '../../components/Header';
 
 test( 'Should render Header correctly', () => {
-    const renderer = new ReactShallowRenderer();
-    
-    renderer.render( <Header /> );
+    const wrapper = shallow( <Header /> );
 
-    expect( renderer.getRenderOutput() ).toMatchSpanshot();
+    expect( wrapper ).toMatchSnapshot();
 } );
