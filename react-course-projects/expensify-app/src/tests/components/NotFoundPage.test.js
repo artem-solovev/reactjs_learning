8eb1bb44478a5718f132b0a02762cabf60259f90
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-07 20:12:44 +0300

    Expensify: updated comment for test

--- react-course-projects/expensify-app/src/tests/components/NotFoundPage.test.js
+++ react-course-projects/expensify-app/src/tests/components/NotFoundPage.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import NotFoundPage from '../../components/NotFoundPage';
 
-test( 'Should render Header correctly', () => {
+test( 'Should render NotFoundPage correctly', () => {
     const wrapper = shallow( <NotFoundPage /> );
 
     expect( wrapper ).toMatchSnapshot();
