dbbed7be31992a0e4ed6a8ab093eb9346767e711
Artem Solovev <artem.solovev.lse@gmail.com>
2018-01-08 12:22:23 +0300

    Expensify: styles moved up

--- react-course-projects/expensify-app/src/app.js
+++ react-course-projects/expensify-app/src/app.js
@@ -14,6 +14,7 @@ import getVisibleExpenses from './selectors/expenses';
 // Styles
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
+import 'react-dates/lib/css/_datepicker.css';
 
 
 /* Main code */
